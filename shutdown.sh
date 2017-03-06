f97851387c1c924f51bceef35fbdb47711f1777f
Andrew Watson <andrewmoorewatson@gmail.com>
2017-03-06 18:02:24 +0000

    shutdown script

---
+++ shutdown.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+raspi-gpio set 14 dl
+raspi-gpio set 15 dl
+raspi-gpio set 16 dl
+raspi-gpio set 17 dl
+raspi-gpio set 18 dl
+raspi-gpio set 19 dl
+raspi-gpio set 20 dl
+raspi-gpio set 21 dl
